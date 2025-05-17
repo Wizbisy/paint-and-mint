e37d750ff049790072b5f7f0664ca0968b7e0350
Wizbisy <155181901+Wizbisy@users.noreply.github.com>
2025-05-17 07:03:29 +0100

    Update Canvas.tsx

--- paint-and-mint/src/components/Canvas.tsx
+++ paint-and-mint/src/components/Canvas.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useEffect, useRef, useState } from "react";
 import p5 from "p5";
-import { sdk } from '@farcaster/frame-sdk';
 import { useAccount, useWriteContract } from "wagmi";
 import { uploadToPinata } from "../utils/pinata";
 import { config } from "../config/wagmi";
@@ -14,7 +13,6 @@ const Canvas = () => {
   const [minting, setMinting] = useState(false);
 
   useEffect(() => {
-    sdk.actions.ready();
     const sketch = (p: p5) => {
       p.setup = () => {
         p.createCanvas(400, 400);
