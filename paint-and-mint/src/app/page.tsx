95eb8f93ed95344399aa0fabb10ff09ab3c467ea
Wizbisy <155181901+Wizbisy@users.noreply.github.com>
2025-05-17 04:46:51 +0100

    Update page.tsx

--- paint-and-mint/src/app/page.tsx
+++ paint-and-mint/src/app/page.tsx
@@ -1,4 +1,6 @@
-import Canvas from "../components/Canvas";
+import dynamic from "next/dynamic";
+
+const Canvas = dynamic(() => import("../components/Canvas"), { ssr: false });
 
 export default function Home() {
   return (
