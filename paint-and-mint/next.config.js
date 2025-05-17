1cec1a3f65a7b6cd0645b1dd2b90a83fdacfaf7d
Wizbisy <155181901+Wizbisy@users.noreply.github.com>
2025-05-17 05:59:44 +0100

    Update next.config.js

--- paint-and-mint/next.config.js
+++ paint-and-mint/next.config.js
@@ -8,6 +8,17 @@ const nextConfig = {
     };
     return config;
   },
+  headers: async () => [
+    {
+      source: "/.well-known/farcaster.json",
+      headers: [
+        {
+          key: "Cache-Control",
+          value: "public, max-age=0, must-revalidate",
+        },
+      ],
+    },
+  ],
 };
 
 module.exports = nextConfig;
